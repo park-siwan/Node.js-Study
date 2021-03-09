72467964e156f910292e2643b43013fc487962e1
siwan <pswan3168@gmail.com>
2021-03-10 01:00:53 +0900

    Express framework learning

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-const http = require('http'); // etc
+// const http = require('http'); // etc
 
-const users = ['Tom', 'Andy', 'Jessica', 'Paul']; // const
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-  // Arrow Function, const
-  if (request.url === '/') {
-    response.end('<h1>Welcome!</h1>');
-  } else if (request.url === '/users') {
-    response.end(`<h1>${users}</h1>`); // Template String
-  } else if (request.url.split('/')[1] === 'users') {
-    // url : /users/1, /users/2, .. // etc
-    const userIdx = request.url.split('/')[2];
-    const userName = users[userIdx - 1]; // etc
+const app = express();
 
-    response.end(`<h1>${userName}</h1>`); // Template String
-  } else {
-    response.end('<h1>Page Not Available</h1>');
-  }
+const users = ['Tom', 'Andy', 'Jessica', 'Paul', 'siwan'];
+
+app.get('/', (request, response) => {
+  response.end('<h1>Welcome!</h1>');
 });
 
-server.listen(3000);
+app.get('/users', (request, response) => {
+  response.end(`<h1>${users}</h1>`);
+});
+
+app.get('/users/:id', (request, response) => {
+  const userName = users[request.params.id - 1];
+  response.end(`<h1>${userName}</h1>`);
+});
+
+app.get('*', (request, response) => {
+  response.end('<h1>Page Not Available</h1>');
+});
+
+app.listen(3000);
