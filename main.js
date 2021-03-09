e802744f4522069aeb81e41dbf00441795e879cf
siwan <pswan3168@gmail.com>
2021-03-09 17:50:06 +0900

    HTTP routing practicing

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-const cowsay = require('cowsay');
+const http = require('http'); // etc
 
-console.log(cowsay.say({ text: 'I love javaScript' }));
+const users = ['Tom', 'Andy', 'Jessica', 'Paul']; // const
+
+const server = http.createServer((request, response) => {
+  // Arrow Function, const
+  if (request.url === '/') {
+    response.end('<h1>Welcome!</h1>');
+  } else if (request.url === '/users') {
+    response.end(`<h1>${users}</h1>`); // Template String
+  } else if (request.url.split('/')[1] === 'users') {
+    // url : /users/1, /users/2, .. // etc
+    const userIdx = request.url.split('/')[2];
+    const userName = users[userIdx - 1]; // etc
+
+    response.end(`<h1>${userName}</h1>`); // Template String
+  } else {
+    response.end('<h1>Page Not Available</h1>');
+  }
+});
+
+server.listen(3000);
